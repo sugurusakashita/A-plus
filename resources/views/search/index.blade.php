17d527ccec55a4d73229d56ad9e094bbc3d4bd9b
Kazuki Bandai <nisigin@gmail.com>
2015-05-30 06:09:38 +0000

    検索結果を綺麗にする

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -9,70 +9,9 @@ WjinkaProj | 検索
 @stop
 
 @section('main_content')
-	<h4>検索結果</h4>
-	<div class="search_header" style="height:200px;margin:30px">
-		<form action="/search" method="get">
-			<table class="table">
-			  <tbody>
-			    <tr>
-			      <td>検索ワード</th>
-			      <td>
-			      	<input type="text" size="20" placeholder="授業名を入力" name="q" value="{{{ $data['get']['q']? $data['get']['q']:"" }}}"/>
-			      	<input type="submit" value="検索">
-			      </td>
-			    </tr>
-			    <tr>
-			      <td>曜日</th>
-			      <td>
-				      <select name="day">
-				      	<option value="0">指定なし</option>
-				      <?php
-				      	$days = ["指定なし","月","火","水","木","金","土","夏季"];
-				      	for($i = 1;$i < 7;$i++){
-
-				      		$str =  "<option value=".$days[$i];
-				      		$str .= $data['get']['day'] == $days[$i]? " selected >":" >";
-				      		$str .= $days[$i]."</option>";
-				      		echo $str;
-				      	}
-				      ?>
-				      </select>
-						</td>
-			    </tr>
-			    <tr>
-			      <td>時限</th>
-			      <td>
-			      	<select name="period">
-			      		<option value="0">指定なし</option>
-			      	<?php
-			      		for($i = 1;$i < 8;$i++){
-			      			$str =  "<option value=".$i;
-			      			$str .= $data['get']['period'] == $i? " selected >":" >";
-			      			$str .= $i."限</option>";
-			      			echo $str;
-			      		}
-			      	?>
-			      	</select>
-			      </td>
-			    </tr>
-			    <tr>
-			      <td>学期</th>
-			      <td>
-			      	<select name="term">
-			      		<option value="2"  {{{ $data['get']['term'] == 2? "selected":"" }}}>指定なし</option>
-			      		<option value="0"  {{{ $data['get']['term'] == 0? " selected":"" }}}>春学期</option>
-			      		<option value="1"  {{{ $data['get']['term'] == 1? " selected":"" }}}>秋学期</option>
-			      	</select>
-			      </td>
-			    </tr>
-			  </tbody>
-			</table>
-			<input type="hidden" name="_token" value="{{csrf_token()}}">
-		</form>
-	</div>
-
-	<div style="text-align:center; margin:30px;">
-	<p><?php
+<div class="panel panel-info">
+	<div class="panel-title">
+	<?php
 		$term = ["春学期 ","秋学期 "];
 		echo $data['get']['term'] == 2? "":$term[$data['get']['term']];
 			if($data['get']['day']){
@@ -84,15 +23,12 @@ WjinkaProj | 検索
 			}
 		echo $data['get']['period']? $data['get']['period']."限 ":"";
 		echo '「'.$data['get']['q'].'」の検索結果'
-		?>
-	</p>
-	<p style="color:#FF0000;"><?php echo $data['classes']->total()? $data['classes']->total()."件ヒットしました！":"";?></p>
-			<?php
-				echo "<p>".$data['classes']->currentPage().' / '.$data['classes']->lastPage()."</p>";
-				echo $data['classes']->render();
-			?>
-		</p>
+	?>
 	</div>
+	<div class="panel-body">
+		<?php echo $data['classes']->total()? $data['classes']->total()."件ヒットしました！":"";?>
+	</div>
+</div>
 	<hr>
 	<?php
 	if($data['classes']->count()){
@@ -121,4 +57,67 @@ WjinkaProj | 検索
 				echo "<p>".$data['classes']->currentPage().' / '.$data['classes']->lastPage()."</p>";
 			?>
 	</div>
+
+	<div class="search_header" style="height:200px;margin:30px">
+		<form action="/search" method="get">
+			<table class="table table-bordered">
+		    <thead>
+			    <tr>
+			      <th>検索ワード</th>
+			      <th>曜日</th>
+			      <th>時限</th>
+			      <th>学期</th>
+		      </tr>
+		    </thead>
+			  <tbody>
+			      <td>
+			      	<div class="form-element-group">
+			      		<input class="form-element" type="text" size="20" placeholder="授業名を入力" name="q" value="{{{ $data['get']['q']? $data['get']['q']:"" }}}"/>
+		      	     <span class="form-group-btn">
+		      	      <button class="btn btn-default btn-primary" type="submit">検索</button>
+		      	     </span>
+	      	    </div>
+			      </td>
+			      <td>
+				      <select name="day">
+				      	<option value="0">指定なし</option>
+				      <?php
+				      	$days = ["指定なし","月","火","水","木","金","土","夏季"];
+				      	for($i = 1;$i < 7;$i++){
+
+				      		$str =  "<option value=".$days[$i];
+				      		$str .= $data['get']['day'] == $days[$i]? " selected >":" >";
+				      		$str .= $days[$i]."</option>";
+				      		echo $str;
+				      	}
+				      ?>
+				      </select>
+						</td>
+			      <td>
+			      	<select name="period">
+			      		<option value="0">指定なし</option>
+			      	<?php
+			      		for($i = 1;$i < 8;$i++){
+			      			$str =  "<option value=".$i;
+			      			$str .= $data['get']['period'] == $i? " selected >":" >";
+			      			$str .= $i."限</option>";
+			      			echo $str;
+			      		}
+			      	?>
+			      	</select>
+			      </td>
+			      <td>
+			      	<select name="term">
+			      		<option value="2"  {{{ $data['get']['term'] == 2? "selected":"" }}}>指定なし</option>
+			      		<option value="0"  {{{ $data['get']['term'] == 0? " selected":"" }}}>春学期</option>
+			      		<option value="1"  {{{ $data['get']['term'] == 1? " selected":"" }}}>秋学期</option>
+			      	</select>
+			      </td>
+			    </tr>
+			  </tbody>
+			</table>
+			<input type="hidden" name="_token" value="{{csrf_token()}}">
+		</form>
+	</div>
+
 @stop
