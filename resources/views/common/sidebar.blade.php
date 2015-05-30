f209bc9769e4f901a62575b3d9c40ec0a5bd445c
Kazuki Bandai <nisigin@gmail.com>
2015-05-30 05:40:04 +0000

    meta,sidebarを共通化

---
+++ resources/views/common/sidebar.blade.php
@@ -0,0 +1,28 @@
+<div class="sidebar">
+  <div class="access_ranking" style="margin-bottom: 15px;">
+    <div class="list-group">
+      <a class="list-group-element active">アクセスランキング</a>
+    <?php
+      $i = 1;
+      foreach ($data['access_ranking'] as $ranking) {
+        // aタグにしてあるから工夫次第でリンク張れる
+        echo "<a class='list-group-element'><span class='badge success'>". $i ."</span>　" . $ranking->class_name."</a>";
+        $i++;
+      }
+    ?>
+    </div>
+  </div>
+
+  <div class="search_word_ranking">
+    <div class="list-group">
+      <a class="list-group-element active">人気検索ワードランキング</a>
+    <?php
+      $i = 1;
+      foreach ($data['search_ranking'] as $ranking) {
+        echo "<a class='list-group-element'><span class='badge success'>". $i ."</span>　".$ranking->word."</a>";
+        $i++;
+      }
+    ?>
+    </div>
+  </div>
+</div>
