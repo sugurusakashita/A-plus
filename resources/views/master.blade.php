c02ef17bb3727fa97e1d737965f4e45ac63ad845
Kazuki Bandai <nisigin@gmail.com>
2015-06-10 02:10:23 +0000

    スタイルが当たってなかったのを修正

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,15 +1,10 @@
 <!doctype>
 <html lang="ja">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+    @include('common.meta')
     <title>
         @yield('title')
     </title>
-    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
-    <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
-    @yield('js')
 </head>
 <body>
   <div class="container">
