92eeefa624506ea62946bd4b1a2865b4608b9bc7
Kazuki Bandai <nisigin@gmail.com>
2015-06-17 06:18:11 +0000

    master.blade.phpのconflict fixをミスりました

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -10,9 +10,9 @@
   <div class="container">
     @include('common.header')
   </div>
-	<div class="container">
-		<div class="search_header" style="margin-top:20px; text-align:center;">
-			<form action="/search" method="get">
+  <div class="container">
+    <div class="search_header" style="margin-top:20px; text-align:center;">
+      <form action="/search" method="get">
         <div class="form-element-group">
           <input type="text" class="form-element" placeholder="授業や講師名で検索！" name="q"/>
           <input type="hidden" name="day" value="0" />
@@ -23,9 +23,20 @@
             <button class="btn btn-default btn-primary" type="submit">検索</button>
           </span>
         </div>
-			</form>
-		</div>
-	</div>
+      </form>
+    </div>
+  </div>
+
+  <div class="container">
+      <div class="row-fluid">
+        <div class="col8">
+          @yield('main_content')
+        </div>
+        <div class="col4">
+          @yield('sidebar')
+        </div>
+      </div>
+  </div>
   @include('common.footer')
 </body>
 </html>
