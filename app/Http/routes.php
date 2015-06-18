1320b682ab1147143a91852464f1a70b23706614
Kazuki Bandai <nisigin@gmail.com>
2015-06-18 06:14:12 +0000

    簡易検索API

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -38,15 +38,22 @@ Route::get('/api/classes/{class_id}', function($class_id){
 
 // 一授業のコメントを取得
 Route::get('/api/classes/{class_id}/reviews/', function($class_id){
-		$class = DB::table('review')
+		$comments = DB::table('review')
 			->where('class_id', $class_id)->get();
-		return Response::json($class);
+		return Response::json($comments);
 });
 
 // 一授業の特定の１コメントを取得
 Route::get('/api/classes/{class_id}/reviews/{rev_id}', function($class_id, $rev_id){
-		$class = DB::table('review')
+		$comment = DB::table('review')
 			->where('class_id', $class_id)
 			->where('review_id', $rev_id)->get();
-		return Response::json($class);
+		return Response::json($comment);
+});
+
+// 簡易検索
+Route::get('/api/search/{word}', function($word){
+		$classes = DB::table('classes')
+			->where('class_name', 'LIKE', '%'.$word.'%')->get();
+		return Response::json($classes);
 });
