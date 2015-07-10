e6aecf20bc03ebd93a92f9dbf20fc4ab9d6e40c8
Kazuki Bandai <nisigin@gmail.com>
2015-07-10 18:25:45 +0000

    refs #1 ロゴ作った

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -2,14 +2,12 @@
   <div class="container">
     <div class="row-fluid">
       <div class="brand">
-        <a href="/" class="">A+plus</a>
+        <a href="/" class=""><img src="/image/Aplus.png" style="width:200px; height:auto;"></a>
       </div>
       <nav class="right-float">
         <a href="/" class="btn-pill">TOPページ</a>
         <a href="{{ url('/search/')}}" class="btn-pill">授業レビュー</a>
 <!--         <a href="{{ url('/search/')}}" class="btn-pill">検索</a> -->
-<!--         <a href="#" class="btn-pill">Link Three</a>
-        <a href="#" class="btn-pill">Link Four</a> -->
         @if (Auth::guest())
           <a href="{{ url('/auth/register') }}" class="btn btn-pill btn-primary-outline">新規登録</a>
           <a href="{{ url('/auth/login') }}" class="btn btn-pill btn-primary-outline">ログイン</a>
@@ -17,7 +15,7 @@
           <a href="/mypage/index" class="btn-pill" >{{ Auth::user()->name }} <span class="caret"></span></a>
           <a href="{{ url('/auth/logout') }}" class="btn btn-pill btn-primary-outline">ログアウト</a>
         @endif
-      </nav> 
+      </nav>
     </div>
 
   </div>
