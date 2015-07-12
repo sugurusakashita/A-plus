afe871318d234630e1e9702f8edc1cccb4ccb8cb
b-kaxa <nisigin@gmail.com>
2015-07-12 19:44:42 +0900

    refs #11 暫定

--- resources/views/top/index.blade.php
+++ resources/views/top/index.blade.php
@@ -5,4 +5,16 @@ A+plus
 @stop
 
 @section('main_content')
+<style type="text/css">
+   body {
+      background-color: #486d46;
+      background-image: url("/image/top_back.jpg");
+      background-size: cover;
+      background-attachment: fixed;
+      background-position: center center;
+   }
+   .nav {
+     background-color: rgba(0,0,0,0);
+   }
+</style>
 @stop
