bc5bd1cc6d926190a4d6e123dd207f93a581252d
b-kaxa <nisigin@gmail.com>
2015-07-20 15:50:58 +0900

    refs #8

--- resources/views/tag/index.blade.php
+++ resources/views/tag/index.blade.php
@@ -23,20 +23,20 @@
 
 @section('main_content')
 	<div class ="row">
+		<!-- タイトル -->
+		<div class="alert a-is-info" style="margin: 0 auto 5px;">
+		 <p>「{{ $data['detail']->class_name }}」にタグを追加する</p>
+		</div>
 
-	<!-- タイトル -->
-	<div class="alert a-is-info" style="margin: 0 auto 5px;">
-	 <p>「{{ $data['detail']->class_name }}」にタグを追加する</p>
+		@foreach($data['tag_names'] as $t)
+			<form action ="/classes/index/{{ $data['detail']->class_id }}" method="POST" name="add_tag">
+				<button class="btn btn-primary" type="submit">{{ $t->tag_name }}</button>
+				<input type="hidden" value="{{ $t->tag_name }}" name="add_tag_name">
+				<input type="hidden" name="_token" value="{{csrf_token()}}" />
+			</form>
+		@endforeach
 	</div>
-
-	@foreach($data['tag_names'] as $t)
-		<form action ="/classes/index/{{ $data['detail']->class_id }}" method="POST" name="add_tag">
-			<button class="btn btn-default" type="submit">{{ $t->tag_name }}</button>
-			<input type="hidden" value="{{ $t->tag_name }}" name="add_tag_name">
-			<input type="hidden" name="_token" value="{{csrf_token()}}" />
-		</form>
-	@endforeach
-
-	<a href="{{ $_SERVER['HTTP_REFERER'] }}"><button class="btn btn-default back">戻る</button></a>
+	<div class ="row">
+		<a href="{{ $_SERVER['HTTP_REFERER'] }}"><button class="btn btn-default back">戻る</button></a>
 	</div>
 @stop
