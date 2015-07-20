8c2d40f136d130a3987f4a653399377783ecfb3d
b-kaxa <nisigin@gmail.com>
2015-07-20 15:47:08 +0900

    refs #8 clean add tag page

--- resources/views/tag/index.blade.php
+++ resources/views/tag/index.blade.php
@@ -1,46 +1,42 @@
 @extends('master')
 
 @section('sidebar')
-@include('common.sidebar')
+	@include('common.sidebar')
+@stop
+
+@section('css')
+<style>
+	form{
+		width: 33%;
+		margin: 10px 0 !important;
+		float: left;
+	}
+	button.back{
+		margin: 15px 0;
+	}
+</style>
 @stop
 
 @section('title')
-タグ追加 | {{ $data['detail']->class_name }}
+	タグ追加 | {{ $data['detail']->class_name }}
 @stop
 
 @section('main_content')
 	<div class ="row">
 
-		<!-- タイトル -->
-		<div class="alert a-is-info" style="margin: 0 auto 5px;">
-		 <p>「{{ $data['detail']->class_name }}」にタグを追加する</p>
-		</div>
+	<!-- タイトル -->
+	<div class="alert a-is-info" style="margin: 0 auto 5px;">
+	 <p>「{{ $data['detail']->class_name }}」にタグを追加する</p>
+	</div>
 
-		<h2>タグ一覧</h2>
-<!--
-	@foreach($data['tag_names'] as $t)
-		<a href="">{{ $t->tag_name }}</a><?php echo "　"?>
-	@endforeach
--->
 	@foreach($data['tag_names'] as $t)
 		<form action ="/classes/index/{{ $data['detail']->class_id }}" method="POST" name="add_tag">
-				<button type="submit">追加する</button>
-				<p>{{ $t->tag_name }}</p>
-				<input type="hidden" value="{{ $t->tag_name }}" name="add_tag_name">
-				<input type="hidden" name="_token" value="{{csrf_token()}}" />
-				<hr>
+			<button class="btn btn-default" type="submit">{{ $t->tag_name }}</button>
+			<input type="hidden" value="{{ $t->tag_name }}" name="add_tag_name">
+			<input type="hidden" name="_token" value="{{csrf_token()}}" />
 		</form>
 	@endforeach
-		<!-- 
-		<table class="table table-striped table-hover">
-			<tbody>
-				<tr>
-				@foreach($data['tag_names'] as $t)
-					<td width="224">{{ $t }}</td>
-				@endforeach
-				</tr>
-			</tbody>
-		</table>
-		-->
+
+	<a href="{{ $_SERVER['HTTP_REFERER'] }}"><button class="btn btn-default back">戻る</button></a>
 	</div>
 @stop
