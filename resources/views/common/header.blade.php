c8e5b88f3321e2801f729e25ea97f75e88f60fe9
b-kaxa <nisigin@gmail.com>
2015-07-30 17:45:54 +0900

    メニューバーの項目調整

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -5,8 +5,8 @@
         <a href="/" style="padding:0;"><img src="{{ asset('image/Aplus_logo_global_trans@2x.png') }}" alt="a+plus_global_logo" width=120></a>
       </div>
       <nav class="right-float" style="padding-top:3px;">
-        <a href="/" class="btn-pill">TOPページ</a>
-        <a href="{{ url('/search/')}}" class="btn-pill">授業レビュー</a>
+<!--         <a href="/" class="btn-pill">TOPページ</a>
+        <a href="{{ url('/search/')}}" class="btn-pill">授業レビュー</a> -->
 <!--         <a href="{{ url('/search/')}}" class="btn-pill">検索</a> -->
         @if (Auth::guest())
           <a href="{{ url('/auth/register') }}" class="btn btn-pill btn-primary-outline">新規登録</a>
