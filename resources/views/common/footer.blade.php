d7160abfcf42c5057a71b59d2c1b131697bd15d3
shalman <nao0101.n@gmail.com>
2015-08-02 22:26:02 +0900

    雛形作成, refs #40

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -1,7 +1,25 @@
-<footer style="margin-top: 30px; margin-bottom: 30px;">
-  <div class="container">
-    <ul class="list-group">
-      <li class="list-group-element">2015 A+plus All Rights Reserved</li>
-    </ul>
-  </div>
+<footer>
+    <span class="col4">
+    	<h3>A+plusについて</h3><hr>
+    	<p>A+plusは2015年5月に設立された早稲田大学非公認アプリケーション開発サークルです。</p>
+    	<ul>
+    		<li><a href="/about">A+plusについて</a></li>
+    		<li><a href="/manual">使い方</a></li>
+    	</ul>
+    	<p>2015 A+plus All Rights Reserved</p>
+    </span>
+    <span class="col4">
+    	<h3>SNS OFFICIAL SITE</h3><hr>
+    	<ul>
+    		<li><a href="https://twitter.com/waseda_Aplus">Twitter</a></li>
+    		<li><a href="">facebook</a></li>
+    	</ul>
+    </span>
+    <span class="col4">
+    	<h3>お問い合わせ</h3><hr>
+    	<ul>
+    		<li><a href="/help">FAQ</a></li>
+    		<li><a href="">お問い合わせ</a></li>
+    	</ul>
+    </span>
 </footer>
