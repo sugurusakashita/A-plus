696a9a3af6914da86f881682d119cd9d35a3a05c
shalman <nao0101.n@gmail.com>
2015-08-07 22:36:58 +0900

    top panel refs, #39

--- resources/views/top/index.blade.php
+++ resources/views/top/index.blade.php
@@ -31,6 +31,13 @@ A+plus
   .left{
     background-color: #F7F7F7;
   }
+
+  .columns{
+    margin: 20px 10px;
+  }
+  .articles{
+    display: inline-block;
+  }
   .article{
     margin: 5%; 
     height: 217px;
@@ -78,75 +85,85 @@ A+plus
   <div class="container left">
   <div class="row-fluid">
       <div class="col9">
-        <h1>News</h1><hr> 
-        <div class="articles">
-          <a href="" class="col4">
-            <div class="article">
-              <img class="article-img" src="{{ asset('image/top_back.jpg') }}" alt="article1" >
-              <div class="article-summary">
-                <label>2015/08/06</label>
-                <label>ほげほげ</label>
-              </div>
-            </div>
-          </a>
-          <a href="" class="col4">
-            <div class="article">
-              <img class="article-img" src="{{ asset('image/bg_top_filtered.jpg') }}" alt="article1" >
-              <div class="article-summary">
-                <label>2015/08/06</label>
-                <label>ほげほげ</label>
-              </div>
-            </div>
-          </a>
-          <a href="" class="col4">
-            <div class="article">
-              <img class="article-img" src="{{ asset('image/Aplus_logo_global@1x.png') }}" alt="article1" >
-              <div class="article-summary">
-                <label>2015/08/06</label>
-                <label>ほげほげ</label>
-              </div>
-            </div>
-          </a>
+        <div class="panel panel-info columns">
+         <div class="panel-title">
+          <h1>News</h1>
         </div>
-        <div class="articles">
-          <a href="" class="col4">
-            <div class="article">
-              <img class="article-img" src="{{ asset('image/top_back.jpg') }}" alt="article1" >
-              <div class="article-summary">
-                <label>2015/08/06</label>
-                <label>ほげほげ</label>
+         <div class="panel-body">
+          <div class="articles">
+            <a href="" class="col4">
+              <div class="article">
+                <img class="article-img" src="{{ asset('image/top_back.jpg') }}" alt="article1" >
+                <div class="article-summary">
+                  <label>2015/08/06</label>
+                  <label>ほげほげ</label>
+                </div>
               </div>
-            </div>
-          </a>
-          <a href="" class="col4">
-            <div class="article">
-              <img class="article-img" src="{{ asset('image/bg_top_filtered.jpg') }}" alt="article1" >
-              <div class="article-summary">
-                <label>2015/08/06</label>
-                <label>ほげほげ</label>
+            </a>
+            <a href="" class="col4">
+              <div class="article">
+                <img class="article-img" src="{{ asset('image/bg_top_filtered.jpg') }}" alt="article1" >
+                <div class="article-summary">
+                  <label>2015/08/06</label>
+                  <label>ほげほげ</label>
+                </div>
               </div>
-            </div>
-          </a>
-          <a href="" class="col4">
-            <div class="article">
-              <img class="article-img" src="{{ asset('image/Aplus_logo_global@1x.png') }}" alt="article1" >
-              <div class="article-summary">
-                <label>2015/08/06</label>
-                <label>ほげほげ</label>
+            </a>
+            <a href="" class="col4">
+              <div class="article">
+                <img class="article-img" src="{{ asset('image/Aplus_logo_global@1x.png') }}" alt="article1" >
+                <div class="article-summary">
+                  <label>2015/08/06</label>
+                  <label>ほげほげ</label>
+                </div>
               </div>
-            </div>
-          </a>
-        </div>  
+            </a>
+          </div>
+          <div class="articles">
+            <a href="" class="col4">
+              <div class="article">
+                <img class="article-img" src="{{ asset('image/top_back.jpg') }}" alt="article1" >
+                <div class="article-summary">
+                  <label>2015/08/06</label>
+                  <label>ほげほげ</label>
+                </div>
+              </div>
+            </a>
+            <a href="" class="col4">
+              <div class="article">
+                <img class="article-img" src="{{ asset('image/bg_top_filtered.jpg') }}" alt="article1" >
+                <div class="article-summary">
+                  <label>2015/08/06</label>
+                  <label>ほげほげ</label>
+                </div>
+              </div>
+            </a>
+            <a href="" class="col4">
+              <div class="article">
+                <img class="article-img" src="{{ asset('image/Aplus_logo_global@1x.png') }}" alt="article1" >
+                <div class="article-summary">
+                  <label>2015/08/06</label>
+                  <label>ほげほげ</label>
+                </div>
+              </div>
+            </a>
+          </div>  
+        </div>
       </div>
+    </div>
       <div class="col3" class="side-content">
-        <h1>sidebar</h1><hr>  
-        <div>
+        <div class="panel panel-primary columns">
+         <div class="panel-title">
+          <h1>sidebar</h1>
+        </div> 
+        <div class="panel-body">
           <img class="article-img" src="{{ asset('image/Aplus_logo_global@1x.png') }}" alt="about_aplus" >
           <ul>
             <li><a href="/auth/facebook-oauth">facebookログイン/新規無料登録</a></li>
           </ul>
         </div> 
       </div>
+    </div>
   </div>
   </div>
 @stop
