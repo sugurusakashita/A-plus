915163e54ac493432890ec44c2e209a60a7ab55c
b-kaxa <nisigin@gmail.com>
2015-08-15 01:33:11 +0900

    footerのデザイン調整。spanはインライン要素なので適していない。

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="container">
         <div class="row-fluid">
-            <span class="col4">
+            <div class="col4">
             	<h3><span class="icon-plus footer-header-icon"></span>A+plusについて</h3><hr>
             	<p>A+plusは2015年5月に設立された早稲田大学非公認アプリケーション開発サークルです。</p>
             	<ul>
@@ -9,20 +9,23 @@
             		<li><a href="/help/manual">使い方</a></li>
                     <li><a href="/term">利用規約および個人情報の利用に関するポリシー</a></li>
             	</ul>
-            	<p>2015 A+plus All Rights Reserved</p>
-            </span>
-            <span class="col4">
+            </div>
+            <div class="col4">
             	<h3><span class="icon-bubbles footer-header-icon"></span>SNS OFFICIAL SITE</h3><hr>
             	<a href="https://twitter.com/waseda_Aplus"><span class="icon-twitter2 footer-sns-icons"></span></a>
             	<a href=""><span class="icon-facebook2 footer-sns-icons"></span></a>
-            </span>
-            <span class="col4">
+            </div>
+            <div class="col4">
             	<h3><span class="icon-envelop footer-header-icon"></span>お問い合わせ</h3><hr>
             	<ul>
             		<li><a href="/help/index">FAQ</a></li>
             		<li><a href="/help/inquiry">お問い合わせ</a></li>
             	</ul>
-            </span>
+            </div>
         </div>
     </div>
+    <div class="allrights">
+        2015 A+plus All Rights Reserved.
+    </div>
+</div>
 </footer>
