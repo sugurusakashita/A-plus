5c554b91a0f8365c29c973c9b26674fdf06b62fb
shalman <nao0101.n@gmail.com>
2015-08-15 16:56:50 +0900

    safari, iOS safariだとヘッダのアイコンが落ちてたので修正

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -3,14 +3,14 @@
     <div class="row-fluid">
       <div class="brand">
         <a href="/" style="padding:0;"><img src="{{ asset('image/Aplus_logo_global_trans@2x.png') }}" alt="a+plus_global_logo" width=120></a>
-          <form action="/search" method="get" style="margin:10px; display:flex" class="left-float">
+          <form action="/search" method="get" class="left-float header-search-form">
             <input type="text" class="form-element-sm form-element" size="40" placeholder="授業名・教師名・キーワードで検索！" name="q" />
             <button class="icon-search search-icon"></button>
-            <input type="hidden" name="_token" value="{{csrf_token()}}">        
+            <input type="hidden" name="_token" value="{{csrf_token()}}">
           </form>
         <!-- </span> -->
       </div>
-      <nav class="right-float" style="padding-top:3px;">        
+      <nav class="right-float" style="padding-top:3px;">
 <!--         <a href="/" class="btn-pill">TOPページ</a>
         <a href="{{ url('/search/')}}" class="btn-pill">授業レビュー</a> -->
 <!--         <a href="{{ url('/search/')}}" class="btn-pill">検索</a> -->
