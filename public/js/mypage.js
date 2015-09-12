84dadba6995547945b06230000f2cd8e406d8254
shalman <nao0101.n@gmail.com>
2015-09-12 19:42:16 +0900

    fix reload bug on safari/iPhone safari

--- public/js/mypage.js
+++ public/js/mypage.js
@@ -19,40 +19,15 @@ jQuery(function ($) {
         var after_button = '<button type="submit" class="btn btn-sm btn-danger btn-xs complete-button right-float">完了</button>';
 
         if(name == "entrance_year"){
-            //ヒアドキュメント風に取得
-            edit_form =　 (function(){/*
-                <select name="entrance_year" class="form-control">
-                    <option value="">選択してください</option>
-                    <option value="その他">2011年度以前</option>
-                    <option value="2012">2012年度(4年生)</option>
-                    <option value="2013">2013年度(3年生)</option>
-                    <option value="2014">2014年度(2年生)</option>
-                    <option value="2015">2015年度(1年生)</option>
-                </select>
-            */}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1];
+            edit_form ='<select name="entrance_year" class="form-control"><option value="">選択してください</option><option value="その他">2011年度以前</option><option value="2012">2012年度(4年生)</option><option value="2013">2013年度(3年生)</option><option value="2014">2014年度(2年生)</option><option value="2015">2015年度(1年生)</option></select>';
         }
 
         if(name == "faculty"){
-            //ヒアドキュメント風に取得
-            edit_form =　 (function(){/*
-            <select name="faculty" class="form-control" }}">
-                <option value="">選択してください</option>
-                <optgroup label="--------学部--------">
-                    <option value="人間科学部">人間科学部</option>
-                    <option value="スポーツ科学部">スポーツ科学部</option>
-                </optgroup>
-            </select>
-            */}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1];
+            edit_form =　'<select name="faculty" class="form-control" }}"><option value="">選択してください</option><optgroup label="--------学部--------"><option value="人間科学部">人間科学部</option><option value="スポーツ科学部">スポーツ科学部</option></optgroup></select>';
         }
 
         if(name == "sex"){
-            //ヒアドキュメント風に取得
-            edit_form =　 (function(){/*
-            <select name="sex" class="form-control" >
-                <option value="男性">男性</option>
-                <option value="女性">女性</option>
-            </select>
-            */}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1];
+            edit_form = '<select name="sex" class="form-control" ><option value="男性">男性</option><option value="女性">女性</option></select>';
         }
 
 
