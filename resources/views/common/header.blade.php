86a9c35e9a5de309382182c97bd6025ef6c65e14
shalman <nao0101.n@gmail.com>
2015-09-26 00:33:12 +0900

    change fb-like to fb-page on side-bar

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -1,3 +1,13 @@
+<!-- facebook SDK START-->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.4&appId=1017018984977481";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<!-- facebook SDK END -->
 <header class="nav">
   <div class="container">
     <div class="row-fluid">
