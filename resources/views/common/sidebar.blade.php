1682e4149c80b3eac583a58e742126cc65d5beaa
shalman <nao0101.n@gmail.com>
2015-10-08 15:29:19 +0900

    add googleAd

--- resources/views/common/sidebar.blade.php
+++ resources/views/common/sidebar.blade.php
@@ -19,6 +19,16 @@
     ?>
     </div>
   </div>
+  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  <!-- side-bar ad -->
+  <ins class="adsbygoogle"
+       style="display:block"
+       data-ad-client="ca-pub-6701768269365502"
+       data-ad-slot="1444343877"
+       data-ad-format="auto"></ins>
+  <script>
+  (adsbygoogle = window.adsbygoogle || []).push({});
+  </script>
   <div class="follow-us">
     <div class="fb-page" data-href="https://www.facebook.com/waseda.Aplus" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" data-show-posts="false"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/waseda.Aplus"><a href="https://www.facebook.com/waseda.Aplus">早稲田大学アプリ開発サークル A+plus</a></blockquote></div></div>
     <a class="twitter-timeline" href="https://twitter.com/waseda_Aplus" data-widget-id="640404274493587456" width="100%">@waseda_Aplusさんのツイート</a>
